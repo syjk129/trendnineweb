149bad34e96535ed402eac0c634ec86602922874
Shawn Kim <syjk129@gmail.com>
2017-12-03 21:39:22 -0800

    post model

---
+++ src/api/models/person.ts
@@ -0,0 +1,10 @@
+export default class Person {
+    id: string;
+    authLevel: number;
+    tier: number;
+    email: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    profileImageUrl: string;
+}
--- src/api/models/post.ts
+++ src/api/models/post.ts
@@ -1,3 +1,12 @@
+import Person from "./person";
+
 export default class Post {
+    id: string;
     title: string;
+    author: Person;
+    modified: string; // TODO: use date
+    products: Array<any>; // TODO: create product model
+    wishlisted: boolean;
+    liked: boolean;
+    likes: number;
 }
