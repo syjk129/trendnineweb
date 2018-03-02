084d2b2b1113eeb8919beb986e0021187f6ad4fc
sheungwoo <selectappuser@gmail.com>
2018-03-01 19:32:46 -0800

    adding card footer

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import * as H from "history";
 import { withRouter } from "react-router-dom";
-
+import TimeAgo from "react-timeago";
 import { Post } from "../../api/models";
 
 import CardContainer from "./cardContainer";
 import Author from "../../components/author";
+import Anchor, {AnchorVariant} from "../../components/anchor";
+import Icon from "../../components/icon";
 import "./style.scss";
 
 interface CardProps {
@@ -15,6 +17,9 @@ interface CardProps {
 class Card extends React.Component<CardProps> {
     render() {
         const { post, history } = this.props;
+        const anchorVariant = post.liked ? AnchorVariant.PRIMARY : AnchorVariant.SECONDARY;
+        const likeText = post.likes === 1 ? "Like" : "Likes";
+
         return (
             <div className="card">
                 <div className="card-cover" onClick={() => history.push(`/post/${post.id}`)}>
@@ -25,7 +30,14 @@ class Card extends React.Component<CardProps> {
                         {post.title}
                     </p>
                     <Author author={post.author} />
-                    <div className="actions">
+                    <div className="card-footer">
+                        <div className="created">
+                            <Icon></Icon>&nbsp;<TimeAgo date={post.created} />
+                        </div>
+                        <div className="action-btns">
+                            <Anchor variant={anchorVariant}><Icon></Icon>&nbsp;{post.likes} {likeText}</Anchor>
+                            <Anchor variant={anchorVariant}><Icon></Icon>&nbsp; Wishlist</Anchor>
+                        </div>
                     </div>
                 </div>
             </div>
