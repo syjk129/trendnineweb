35835db0ea4ccbcd24cad232c62b34270727c365
sheungwoo <selectappuser@gmail.com>
2018-03-01 17:57:06 -0800

    added in variant placeholder for icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -2,15 +2,26 @@ import * as React from "react";
 
 import "./style.scss";
 
+export enum IconVariant {
+    // TODO: add in variants
+}
+
 interface IconProps {
     className?: string;
+    variant?: IconVariant;
     children?: React.ReactNode;
 }
 
-export default function Icon({ className, children }: IconProps) {
+export default function Icon({ className, variant, children }: IconProps) {
     let classes = "icon";
     if (className) {
         classes += ` ${className}`;
+    }
+
+    // TODO: add variant to set icons
+    switch (variant) {
+        default:
+            break;
     }
 
     return (
