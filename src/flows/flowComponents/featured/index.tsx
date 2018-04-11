65dce326aec9d8c477a67539a00b5acc7461282a
Shawn Kim <syjk129@gmail.com>
2018-04-10 23:26:28 -0700

    link styling

--- src/flows/flowComponents/featured/index.tsx
+++ src/flows/flowComponents/featured/index.tsx
@@ -1,4 +1,6 @@
+import * as H from "history";
 import * as React from "react";
+import { withRouter } from "react-router-dom";
 
 import { FeaturedInfleuncer } from "../../../api/models";
 import Button, { ButtonVariant } from "../../../components/button";
@@ -8,15 +10,17 @@ import "./style.scss";
 
 interface FeaturedProps {
     featuredTrendnines: Array<FeaturedInfleuncer>;
+    history: H.History;
 }
 
-export default class Featured extends React.Component<FeaturedProps> {
+class Featured extends React.Component<FeaturedProps> {
     render() {
+        const { featuredTrendnines, history } = this.props;
         return (
             <SidebarSection title="Today's Trendnine">
                 {this.props.featuredTrendnines.slice(0, 2)
                     .map(trendnine => (
-                        <div className="featured-trendnine">
+                        <div className="featured-trendnine" onClick={() => history.push(`/user/${trendnine.user.ussername}`)}>
                             <img src={trendnine.user.profile_image_url} />
                             <div className="trendnine-details">
                                 <p className="trendnine-name">
@@ -32,3 +36,5 @@ export default class Featured extends React.Component<FeaturedProps> {
         );
     }
 }
+
+export default withRouter(Featured);
