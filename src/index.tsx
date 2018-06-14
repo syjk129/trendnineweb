175f4489cf96a8f0268e47365bfc06f03c5dc5dd
Shawn Kim <syjk129@gmail.com>
2018-06-14 13:55:11 -0700

    oops

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import * as Raven from "raven-js";
 import * as React from "react";
 import {render} from "react-dom";
 import {AppContainer} from "react-hot-loader";
 
 import App from "./app";
-
-Raven.config("https://db18bddd4de44973ae76f0c6c05f0b8d@sentry.io/1226672", {
-    release: "0-0-0",
-    environment: "development-test",
-}).install();
 
 const rootEl = document.getElementById("root");
 
