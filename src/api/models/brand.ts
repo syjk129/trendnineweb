309f4b0c3e7b9afe3de032d41ab54543c09cc351
sheungwoo <bryan@trendnine.com>
2018-06-23 22:57:16 -0400

    added brands tab

---
+++ src/api/models/brand.ts
@@ -0,0 +1,5 @@
+export default class Brand {
+    id: string;
+    name: string;
+    item_count: number;
+}
---
+++ src/components/scrollTo/index.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+
+interface ScrollToProps {
+    id: string;
+    offsetY?: number;
+}
+
+export default function ScrollTo({ id, offsetY }: ScrollToProps) {
+    let settings = {
+        duration: 500,
+        easing: {
+            outQuint: function (x, t, b, c, d) {
+                return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+            },
+        },
+    };
+
+    let percentage;
+    let startTime;
+    const node = document.getElementById(id);
+
+    if (!node) {
+        return;
+    }
+    const nodeTop = node.offsetTop;
+    const nodeHeight = node.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const height = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight,
+    );
+    const windowHeight = window.innerHeight;
+    const offset = window.pageYOffset;
+    const delta = nodeTop - offset - offsetY;
+    const bottomScrollableY = height - windowHeight;
+    const targetY = (bottomScrollableY < delta) ?
+        bottomScrollableY - (height - nodeTop - nodeHeight + offset) : delta;
+
+    startTime = Date.now();
+    percentage = 0;
+
+    if (this.timer) {
+        clearInterval(this.timer);
+    }
+
+    const step = () => {
+        let yScroll;
+        let elapsed = Date.now() - startTime;
+
+        if (elapsed > settings.duration) {
+            clearTimeout(this.timer);
+        }
+
+        percentage = elapsed / settings.duration;
+
+        if (percentage > 1) {
+            clearTimeout(this.timer);
+        } else {
+            yScroll = settings.easing.outQuint(0, elapsed, offset, targetY, settings.duration);
+            window.scrollTo(0, yScroll);
+            this.timer = setTimeout(step, 10);
+        }
+    };
+
+    this.timer = setTimeout(step, 10);
+}
