99c7259b50a3bb724136bedc0ceff85d241f51b3
Shawn Kim <syjk129@gmail.com>
2018-06-27 09:22:27 -0400

    typescript 2.9 breaking changes

--- src/app/withUserSession.tsx
+++ src/app/withUserSession.tsx
@@ -4,7 +4,7 @@ import { Person } from "../api/models";
 
 type Diff<T extends string, U extends string> = ({ [P in T]: P } &
     { [P in U]: never } & { [x: string]: never })[T];
-type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+type Omit<T, K extends keyof T> = Pick<T, Diff<Extract<keyof T, string>, Extract<K, string>>>;
 
 interface WithUserSessionProvides {
     user: Person;
