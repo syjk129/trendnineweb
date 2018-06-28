70304e467d489275524a6e31864c0a2257fe24ad
Shawn Kim <syjk129@gmail.com>
2018-06-28 14:10:26 -0400

    fix

--- src/flows/post/desktop.tsx
+++ src/flows/post/desktop.tsx
@@ -61,7 +61,7 @@ export default class DesktopPost extends React.Component<DesktopPostProps, Deskt
         // remove this later
         this._coverImageRef.current.addEventListener("click", (event) => {
             console.log(`x: ${(event.clientX - rect.left) / rect.width}`);
-            console.log(`y: ${(event.clientY - rect.top) / rect.width}`);
+            console.log(`y: ${(event.clientY - rect.top) / rect.height}`);
         });
         const user = localStorage.getItem("user");
         const token = localStorage.getItem("tn_auth_token");
