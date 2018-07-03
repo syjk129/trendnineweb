6499b50e5dca48545b54e5a238f4222de023d578
Shawn Kim <syjk129@gmail.com>
2018-07-03 10:35:19 -0400

    forgot a file

---
+++ src/flows/flowComponents/share/facebookShare.ts
@@ -0,0 +1,37 @@
+class FacebookLoginResponse {
+    code: string;
+    error?: string;
+}
+
+const CALLBACK_URL_PATH = "/facebook/callback";
+
+export default function FacebookShare(appId: string, pathname: string, callBack: (response: FacebookLoginResponse) => void) {
+    const width = 450;
+    const height = 300;
+    const x = screen.width / 2 - width / 2;
+    const y = screen.height / 2 - height / 2;
+
+    let url = "https://www.facebook.com/dialog/share?" +
+        `app_id=${appId}` +
+        `&display=popup` +
+        `&href=beta.trendnine.com/${pathname}` +
+        `&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer`;
+
+    let externalWindow = window.open(url, "_blank", `width=${width},height=${height},left=${x},top=${y},status=yes`);
+    this._detectUrlChange(externalWindow, callBack);
+}
+
+function _detectUrlChange(externalWindow: Window, callbackOnLogin: (response: FacebookLoginResponse) => void) {
+    const interval = setInterval(function() {
+        if (externalWindow.location.pathname && externalWindow.location.pathname.endsWith(CALLBACK_URL_PATH)) {
+            const code = new URL(externalWindow.location.href).searchParams.get("code");
+
+            // call provided callback function
+            callbackOnLogin({ code: code });
+
+            // clean interval and close window.
+            clearInterval(interval);
+            externalWindow.close();
+        }
+    }, 500);
+}
