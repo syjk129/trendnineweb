f38094396a5b19202ba1db8d72b119592a63edfa
Shawn Kim <syjk129@gmail.com>
2018-07-03 08:48:55 -0400

    testing meta tags

--- src/flows/discover/index.tsx
+++ src/flows/discover/index.tsx
@@ -19,6 +19,11 @@ export default class Discover extends React.Component<Props> {
 
         return (
             <div>
+                <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+                <meta property="og:type" content="article" />
+                <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+                <meta property="og:description" content="How much does culture influence creative thinking?" />
+                <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
                 <PageNavigation />
                 <BrowserView device={isBrowser}>
                     <DesktopDiscover
