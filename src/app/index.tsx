f3c28c9fe2f47f3ab43498933582a366a422b138
Shawn Kim <syjk129@gmail.com>
2018-07-06 11:55:04 -0400

    add polyfill for es6 promise

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
+import { polyfill } from "es6-promise";
 import * as React from "react";
 import { browserHistory, BrowserRouter, Route } from "react-router-dom";
 
 import App from "./app";
+
+polyfill();
 
 export default function AppContainer() {
     return (
