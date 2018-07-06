df781bf99ea35a06fac816065d5d75015a72f03b
Shawn Kim <syjk129@gmail.com>
2018-07-05 23:44:29 -0400

    fix footer border

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -32,41 +32,43 @@ export default class Footer extends React.Component<Props, FooterState> {
         const isShop = this.props.location.pathname.indexOf("/shop") > -1;
 
         return (
-            <div className="footer" id="footer">
-                <div className="footer-logo">
-                </div>
-                <div className="footer-content">
-                    <div className="footer-column">
-                        <p className="footer-title">Trendnine</p>
-                        <Link className="footer-link" to={isShop ? "/shop/about" : "/about"}>About Us</Link>
-                        <Link className="footer-link" to={isShop ? "/shop/opportunities" : "/opportunities"}>I'm an Influencer</Link>
-                        <Link className="footer-link" to={isShop ? "/shop/terms" : "/terms"}>Terms &amp; Conditions</Link>
-                        <Link className="footer-link" to={isShop ? "/shop/privacy" : "/privacy"}>Privacy Policy</Link>
-                        <Link className="footer-link" to={isShop ? "/shop/contact" : "/contact"}>Contact Us</Link>
+            <div className="footer-container">
+                <div className="footer" id="footer">
+                    <div className="footer-logo">
                     </div>
-                    <div className="footer-column">
-                        <p className="footer-title">Follow us</p>
-                        <div className="social-buttons">
-                            <LinkButton href="https://www.instagram.com/trendnine/" target="_blank"><SocialIcon icon={SocialIconType.INSTAGRAM} /></LinkButton>
-                            <LinkButton href="https://www.facebook.com/trendnine" target="_blank"><SocialIcon icon={SocialIconType.FACEBOOK} /></LinkButton>
-                            <LinkButton href="https://twitter.com/trendnine" target="_blank"><SocialIcon icon={SocialIconType.TWITTER} /></LinkButton>
-                            <LinkButton href="https://www.pinterest.com/trendnine/" target="_blank"><SocialIcon icon={SocialIconType.PINTEREST} /></LinkButton>
+                    <div className="footer-content">
+                        <div className="footer-column">
+                            <p className="footer-title">Trendnine</p>
+                            <Link className="footer-link" to={isShop ? "/shop/about" : "/about"}>About Us</Link>
+                            <Link className="footer-link" to={isShop ? "/shop/opportunities" : "/opportunities"}>I'm an Influencer</Link>
+                            <Link className="footer-link" to={isShop ? "/shop/terms" : "/terms"}>Terms &amp; Conditions</Link>
+                            <Link className="footer-link" to={isShop ? "/shop/privacy" : "/privacy"}>Privacy Policy</Link>
+                            <Link className="footer-link" to={isShop ? "/shop/contact" : "/contact"}>Contact Us</Link>
                         </div>
-                    </div>
-                    <div className="footer-disclaimer">
-                        <p className="footer-title">Don't Miss Out</p>
-                        <p>Get exclusive offers, inspiration, and trend updates delivered right to your inbox.</p>
-                        <form onSubmit={this._onSubmit}>
-                            <Input
-                                variant={InputVariant.OUTLINE}
-                                placeholder="Email"
-                                value={this.state.email}
-                                onChange={this._onInputChange}
-                                type={InputType.EMAIL} required={true} />
-                            <Input type={InputType.SUBMIT} value="SUBSCRIBE"></Input>
-                        </form>
-                        {this.state.success && (<p>Subscribed!</p>)}
-                        <p className="disclaimer">© 2018 TrendNine, Inc. All rights reserved.</p>
+                        <div className="footer-column">
+                            <p className="footer-title">Follow us</p>
+                            <div className="social-buttons">
+                                <LinkButton href="https://www.instagram.com/trendnine/" target="_blank"><SocialIcon icon={SocialIconType.INSTAGRAM} /></LinkButton>
+                                <LinkButton href="https://www.facebook.com/trendnine" target="_blank"><SocialIcon icon={SocialIconType.FACEBOOK} /></LinkButton>
+                                <LinkButton href="https://twitter.com/trendnine" target="_blank"><SocialIcon icon={SocialIconType.TWITTER} /></LinkButton>
+                                <LinkButton href="https://www.pinterest.com/trendnine/" target="_blank"><SocialIcon icon={SocialIconType.PINTEREST} /></LinkButton>
+                            </div>
+                        </div>
+                        <div className="footer-disclaimer">
+                            <p className="footer-title">Don't Miss Out</p>
+                            <p>Get exclusive offers, inspiration, and trend updates delivered right to your inbox.</p>
+                            <form onSubmit={this._onSubmit}>
+                                <Input
+                                    variant={InputVariant.OUTLINE}
+                                    placeholder="Email"
+                                    value={this.state.email}
+                                    onChange={this._onInputChange}
+                                    type={InputType.EMAIL} required={true} />
+                                <Input type={InputType.SUBMIT} value="SUBSCRIBE"></Input>
+                            </form>
+                            {this.state.success && (<p>Subscribed!</p>)}
+                            <p className="disclaimer">© 2018 TrendNine, Inc. All rights reserved.</p>
+                        </div>
                     </div>
                 </div>
             </div>
