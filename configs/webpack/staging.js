4401e9b337dd75fe486f96c07f6bda7e85414476
Shawn Kim <syjk129@gmail.com>
2018-07-27 16:40:07 -0400

    require

--- configs/webpack/staging.js
+++ configs/webpack/staging.js
@@ -1,6 +1,7 @@
 // staging config
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {resolve} = require('path');
 
 const commonConfig = require('./common');
