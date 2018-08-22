1a073eae843359a4c8b2c02d503ae0c013f670e7
Shawn Kim <syjk129@gmail.com>
2018-08-22 18:22:56 -0400

    style the product search modal

--- src/flows/cms/productSearchModal/index.tsx
+++ src/flows/cms/productSearchModal/index.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 
 import { ProductSearchResult } from "../../../api/models";
 import { AppContext } from "../../../app";
-import Button from "../../../components/button";
-import Image from "../../../components/image";
+import Button, { ButtonSize, ButtonVariant } from "../../../components/button";
+import Image, { ImageFitVariant, ImageRatioVariant } from "../../../components/image";
 import Input from "../../../components/input";
 import Modal from "../../../components/modal";
 import { ProductSearchTag } from "../types";
@@ -41,42 +41,77 @@ export default class ProductSearchModal extends React.Component<ProductSearchMod
                     <Modal isOpen={this.state.modalOpen} close={this._closeModal} className="product-tag-modal">
                         <div className="product-search-modal-container">
                             <div className="product-search">
-                                <Input value={this.state.searchTerm} onChange={this._onSearchTermChange} />
-                                <Button onClick={this._search}>Search</Button>
+                                <form
+                                    className="product-search-input"
+                                    onSubmit={this._onFormKeyDown}
+                                >
+                                    <Input value={this.state.searchTerm} onChange={this._onSearchTermChange} />
+                                    <Button inline variant={ButtonVariant.OUTLINE} onClick={this._search}>Search</Button>
+                                </form>
                                 <div className="product-search-results">
                                     {this.state.searchResults.map(result => (
                                         <div className="product-search-result">
-                                            <Image className="product-image" src={result.image} />
+                                            <Image fit={ImageFitVariant.SCALED} ratio={ImageRatioVariant.PRODUCT_IMAGE} className="product-image" src={result.image} />
                                             <div className="product-search-result-detail">
                                                 <div className="product-search-label">
                                                     Brand
                                                 </div>
-                                                {result.brand}
+                                                <div className="product-search-info">{result.brand}</div>
                                                 <div className="product-search-label">
                                                     Retailer
                                                 </div>
-                                                {result.merchant}
+                                                <div className="product-search-info">{result.merchant}</div>
                                                 <div className="product-search-label">
                                                     Name
                                                 </div>
-                                                {result.title}
+                                                <div className="product-search-info">{result.title}</div>
                                                 <div className="product-search-label">
                                                     Price
                                                 </div>
-                                                {result.price}
-                                                <Button onClick={() => this.props.selectProduct(result)}>Add Product</Button>
+                                                <div className="product-search-info">{result.price}</div>
+                                                <div className="divider" />
+                                                <Button
+                                                    variant={ButtonVariant.OUTLINE}
+                                                    size={ButtonSize.VERY_SMALL}
+                                                    onClick={() => this.props.selectProduct(result)}
+                                                >
+                                                    Add
+                                                </Button>
                                             </div>
                                         </div>
                                     ))}
                                 </div>
                             </div>
                             <div className="tagged-products">
-                                Tagged Products
+                                <div className="product-search-title">Tagged Products</div>
                                 {this.props.selectedProducts.map(selectedProduct => (
-                                    <div className="tagged-product">
-                                        {selectedProduct.title}
-                                        <Button inline onClick={() => this.props.removeProduct(selectedProduct)}>Remove</Button>
-                                    </div>
+                                    <>
+                                        <div className="tagged-product">
+                                            <img className="tagged-product-img" src={selectedProduct.image} />
+                                            <div>
+                                                <div className="product-search-label">
+                                                    Brand
+                                                </div>
+                                                <div className="product-search-info">{selectedProduct.brand}</div>
+                                                <div className="product-search-label">
+                                                    Title
+                                                </div>
+                                                <div className="product-search-info">{selectedProduct.title}</div>
+                                                <div className="product-search-label">
+                                                    Price
+                                                </div>
+                                                <div className="product-search-info">{selectedProduct.price}</div>
+                                            </div>
+                                        </div>
+                                        <Button
+                                            inline
+                                            variant={ButtonVariant.OUTLINE}
+                                            size={ButtonSize.VERY_SMALL}
+                                            onClick={() => this.props.removeProduct(selectedProduct)}
+                                        >
+                                            Remove
+                                        </Button>
+                                    </>
                                 ))}
                             </div>
                         </div>
@@ -86,9 +121,16 @@ export default class ProductSearchModal extends React.Component<ProductSearchMod
         );
     }
 
+    private _onFormKeyDown = (event) => {
+        event.preventDefault();
+        this._search();
+        return false;
+    }
+
     private _search = async () => {
         const searchResults = await this.context.api.searchProducts(`?keywords=${this.state.searchTerm}`);
         this.setState({ searchResults });
+        return false;
     }
 
     private _onSearchTermChange = (searchTerm: string) => {
