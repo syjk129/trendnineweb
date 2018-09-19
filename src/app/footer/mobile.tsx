c5f339aa4608b5c9e556655324e5ca6e44c21e18
Shawn Kim <syjk129@gmail.com>
2018-09-19 17:13:14 -0400

    add newsletter signup

--- src/app/footer/mobile.tsx
+++ src/app/footer/mobile.tsx
@@ -4,6 +4,7 @@ import * as LogoWhite from "../logo_white.png";
 
 import { SocialIcon, SocialIconType } from "../../components/icon";
 import Image from "../../components/image";
+import Input, { InputVariant } from "../../components/input";
 
 interface FooterProps {
     email: string;
@@ -14,22 +15,40 @@ interface FooterProps {
 
 export default function MobileFooter({ email, success, onEmailChange, submit }: FooterProps) {
     return (
-        <div className="mobile-footer">
-            <div className="follow-us-text">
-                Follow us on
+        <>
+            <div className="mobile-newsletter">
+                <b>🔥Never miss out on us🔥</b>
+                <div className="newsletter-info">
+                    Sign up to receive TrendNine emails and never miss out on the hottest trends in Social media and more!
+                </div>
+                <form onSubmit={submit}>
+                    <Input
+                        className="newsletter-email"
+                        variant={InputVariant.BLANK}
+                        value={email}
+                        onChange={onEmailChange}
+                        placeholder="Enter your email here"
+                    />
+                </form>
+                {success && (<p>Subscribed!</p>)}
             </div>
-            <div className="follow-us-buttons">
-                <SocialIcon icon={SocialIconType.PINTEREST} white />
-                <SocialIcon icon={SocialIconType.FACEBOOK} white />
-                <SocialIcon icon={SocialIconType.INSTAGRAM} white />
+            <div className="mobile-footer">
+                <div className="follow-us-text">
+                    Follow us on
+                </div>
+                <div className="follow-us-buttons">
+                    <SocialIcon icon={SocialIconType.PINTEREST} white />
+                    <SocialIcon icon={SocialIconType.FACEBOOK} white />
+                    <SocialIcon icon={SocialIconType.INSTAGRAM} white />
+                </div>
+                <Link className="footer-link" to="/about">About Us</Link>
+                <Link className="footer-link" to="/opportunities">I'm an Influencer</Link>
+                <Link className="footer-link" to="/terms">Terms &amp; Conditions</Link>
+                <Link className="footer-link" to="/privacy">Privacy Policy</Link>
+                <Link className="footer-link" to="/contact">Contact Us</Link>
+                <Image className="footer-logo" src={LogoWhite} />
+                <div className="copyright">©TrendNine All Rights Reserved</div>
             </div>
-            <Link className="footer-link" to="/about">About Us</Link>
-            <Link className="footer-link" to="/opportunities">I'm an Influencer</Link>
-            <Link className="footer-link" to="/terms">Terms &amp; Conditions</Link>
-            <Link className="footer-link" to="/privacy">Privacy Policy</Link>
-            <Link className="footer-link" to="/contact">Contact Us</Link>
-            <Image className="footer-logo" src={LogoWhite} />
-            <div className="copyright">©TrendNine All Rights Reserved</div>
-        </div>
+        </>
     );
 }
